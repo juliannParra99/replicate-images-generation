47f37376662ef7490cbfa7d7d04e2a3f93168fc1
julianParrax <julianfranco.parra@gmail.com>
2024-01-29 05:01:37 -0300

    Added: 1.3.1- Logical separation between client and server to handle HTTP POST operations on the server for creating predictions with specific prompts for the model image, and on the client to fetch states and progressthrough HTTP GET requests to obtan the odel im image. This facilitates drawing data on the frontend. Atention: I must fix the Skeleton when I make a new promt, to make that the Skeleton replace the Image meanwhile we get the new one; otherwise the skeleton will show it under the curren image

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,22 @@
+// the "use client" section focuses on handling form state and user interface elements. Through the use of the useFormState and useFormStatus hooks, managing form state and obtaining information about submission status is streamlined. The handleSubmit function serves as a bridge between client-side logic and server-side functions, orchestrating form submission and dynamically updating the user interface based on prediction progress. This seamless integration between the client and server enables a cohesive and responsive user experience in the application.
 "use client";
 
 import Image from "next/image";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Textarea } from "@/components/ui/textarea";
-import { createPrediction } from "@/actions";
+import { createPrediction, getPrediction } from "@/actions";
 import { useFormState, useFormStatus } from "react-dom";
+import { Prediction } from "@/types";
+import { Skeleton } from "@/components/ui/skeleton";
+
+const sleep = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
 
 function FormContent() {
-  const {pending} = useFormStatus();
+  const { pending } = useFormStatus();
   return (
     <>
+      {pending ? <Skeleton className="h-[480px] w-[512px]"></Skeleton> : null}
       <Input
         placeholder="https://replicate.delivery/pbxt/IJZOELWrncBcjdE1s5Ko8ou35ZOxjNxDqMf0BhoRUAtv76u4/room.png"
         defaultValue="https://replicate.delivery/pbxt/IJZOELWrncBcjdE1s5Ko8ou35ZOxjNxDqMf0BhoRUAtv76u4/room.png"
@@ -24,8 +30,17 @@ function FormContent() {
 }
 
 export default function Home() {
-  //i pass it my server action; with this, i recover the data from my server component, and i can use it in the client
-  const [state, formAction] = useFormState(createPrediction, null);
+  const [state, formAction] = useFormState(handleSubmit, null)
+  async function handleSubmit(_state: null | Prediction,formData: FormData) {
+    let prediction = await createPrediction(formData);
+
+    while (["starting", "processing"].includes(prediction.status)) {
+      prediction = await getPrediction(prediction.id);
+
+      await sleep(4000);
+    }
+    return prediction
+  }
 
   return (
     <section className="m-auto grid gap-4 max-w-[512px]">
